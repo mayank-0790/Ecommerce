28917a63e7b706243416ce57eec99b9105f98add
mayank-0790 <mayank2311052@akgec.ac.in>
2025-11-06 10:29:32 +0530

    Ecommerce

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,6 +1,12 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+
+    @Id
     private Long categoryId;
     private String categoryName;
 
