4ebc13a8b22db443b14ae15208ce8c1fa54f410e
mayank-0790 <mayankrajput2100@gmail.com>
2025-11-07 19:30:26 +0530

    added getter and setter

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -19,6 +19,19 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
+    public Long getCategoryId() {
+        return categoryId;
+    }
 
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
 
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
